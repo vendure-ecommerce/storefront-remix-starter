8d1f3a30fd2ef8de1abd23315956f8fa06a1699f
Michael Bromley <michael@michaelbromley.co.uk>
2022-05-05 22:18:50 +0200

    chore: Netlify setup #4

--- app/sessions.ts
+++ app/sessions.ts
@@ -1,15 +1,11 @@
 import { createCookieSessionStorage } from '@remix-run/node';
 
-if (!process.env.ENCRYPTION_KEY) {
-  throw new Error("ENCRYPTION_KEY environment variable is not set");
-}
-
 export const sessionStorage = createCookieSessionStorage({
   cookie: {
     name: "session",
     httpOnly: true,
     path: "/",
     sameSite: "lax",
-    secrets: [process.env.ENCRYPTION_KEY],
+    secrets: [process.env.ENCRYPTION_KEY ?? 'awdbhbjahdbaw'],
   },
 });
