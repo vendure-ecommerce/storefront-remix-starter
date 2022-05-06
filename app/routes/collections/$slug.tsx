fe7f2b78b9b543b2b404e9fb0645a07ea900f781
Michael Bromley <michael@michaelbromley.co.uk>
2022-05-06 09:51:09 +0200

    fix: Group category products

--- app/routes/collections/$slug.tsx
+++ app/routes/collections/$slug.tsx
@@ -8,44 +8,50 @@ import { Breadcrumbs } from '~/components/Breadcrumbs';
 
 export type CollectionWithProducts = Awaited<ReturnType<typeof loader>>;
 
-export async function loader({ params }: DataFunctionArgs) {
-  const { search: { items: products }} = await search({ input: { collectionSlug: params.slug, take: 999 } });
-  const collection = (await sdk.collection({ slug: params.slug })).collection;
-  if (!collection?.id || !collection?.name) throw "Collection not found";
+export async function loader({params}: DataFunctionArgs) {
+    const {search: {items: products}} = await search({
+        input: {
+            collectionSlug: params.slug,
+            take: 999,
+            groupByProduct: true
+        }
+    });
+    const collection = (await sdk.collection({slug: params.slug})).collection;
+    if (!collection?.id || !collection?.name) throw "Collection not found";
 
-  return {
-    collection,
-    products,
-  };
+    return {
+        collection,
+        products,
+    };
 }
 
 export default function CollectionSlug() {
-  const { collection, products } = useLoaderData<CollectionWithProducts>();
-  return (<div className="max-w-6xl mx-auto px-4">
+    const {collection, products} = useLoaderData<CollectionWithProducts>();
+    return (<div className="max-w-6xl mx-auto px-4">
         <h2 className="text-5xl font-light tracking-tight text-gray-900 my-8">
-          {collection.name}
+            {collection.name}
         </h2>
 
         <Breadcrumbs items={collection.breadcrumbs}></Breadcrumbs>
         {collection.children?.length ? (
-          <div className="max-w-2xl mx-auto px-4 py-16 sm:py-16 lg:max-w-none">
-            <h2 className="text-2xl font-light text-gray-900">Collections</h2>
-            <div className="mt-6 grid sm:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-4">
-              {collection.children.map(child => (
-                <CollectionCard collection={child}></CollectionCard>
-              ))}
+            <div className="max-w-2xl mx-auto px-4 py-16 sm:py-16 lg:max-w-none">
+                <h2 className="text-2xl font-light text-gray-900">Collections</h2>
+                <div className="mt-6 grid sm:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-4">
+                    {collection.children.map(child => (
+                        <CollectionCard collection={child}></CollectionCard>
+                    ))}
+                </div>
             </div>
-          </div>
         ) : (
-          ""
+            ""
         )}
 
         <div className="max-w-2xl mx-auto py-16 px-4 lg:max-w-6xl">
-          <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-            {products.map(item => (
-              <ProductCard {...item}></ProductCard>
-            ))}
-          </div>
+            <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+                {products.map(item => (
+                    <ProductCard {...item}></ProductCard>
+                ))}
+            </div>
         </div>
-      </div>)
+    </div>)
 }
