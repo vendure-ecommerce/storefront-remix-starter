91f14bc379a3716368506d61edcea2db805823eb
Michael Bromley <michael@michaelbromley.co.uk>
2022-05-09 10:04:21 +0200

    fix: Key warning on search page

--- app/routes/search.tsx
+++ app/routes/search.tsx
@@ -30,7 +30,7 @@ export default function Search() {
         <div className="max-w-2xl mx-auto py-16 px-4 lg:max-w-6xl">
             <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
                 {result.items.map(item => (
-                    <ProductCard {...item}></ProductCard>
+                    <ProductCard key={item.productId} {...item}></ProductCard>
                 ))}
             </div>
         </div>
