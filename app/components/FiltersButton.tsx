3dc44be60be72c95ebe050f0a712dac20e97fabf
Michael Bromley <michael@michaelbromley.co.uk>
2022-05-20 17:14:14 +0200

    fix: Fix filter button zero state

--- app/components/FiltersButton.tsx
+++ app/components/FiltersButton.tsx
@@ -1,18 +1,27 @@
 import { FilterIcon } from '@heroicons/react/solid';
 
-export function FiltersButton({filterCount, onClick}: { filterCount: number; onClick: () => void }) {
+export function FiltersButton({
+    filterCount,
+    onClick,
+}: {
+    filterCount: number;
+    onClick: () => void;
+}) {
     return (
         <button
             type="button"
             className="flex space-x-2 items-center border rounded p-2 ml-4 sm:ml-6 text-gray-400 hover:text-gray-500 lg:hidden"
             onClick={onClick}
         >
-            {filterCount && <span
-                className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-indigo-200 text-indigo-800">
+            {!!filterCount ? (
+                <span className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-indigo-200 text-indigo-800">
                     {filterCount}
-                  </span>}
+                </span>
+            ) : (
+                ''
+            )}
             <span>Filters</span>
-            <FilterIcon className="w-5 h-5" aria-hidden="true"/>
+            <FilterIcon className="w-5 h-5" aria-hidden="true" />
         </button>
-    )
+    );
 }
