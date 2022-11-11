6781269e7be9e84727715e206edf91a989abe1a3
Hendrik Mennen <hmennen@protop-solutions.com>
2022-11-11 08:31:09 +0100

    .

--- app/constants.ts
+++ app/constants.ts
@@ -1,2 +1,3 @@
 export const APP_META_TITLE = 'VHDPlus Shop';
 export const APP_META_DESCRIPTION = 'High quality hardware and software developed in Germany';
+export const DEMO_API_URL = 'https://readonlydemo.vendure.io/shop-api';
