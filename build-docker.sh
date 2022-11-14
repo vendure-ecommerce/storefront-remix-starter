dd017105357634d326d71cc27497556e6715add8
Hendrik Mennen <hendrik.mennen@hs-osnabrueck.de>
2022-11-14 16:47:46 +0100

    Update build-docker.sh

--- build-docker.sh
+++ build-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker build -t eu.gcr.io/vhdplus-shop/storefront .
+docker build -t eu.gcr.io/vhdplus/storefront .
 # Configure docker to use Google authentication
 gcloud auth configure-docker -q
-docker push eu.gcr.io/vhdplus-shop/storefront
+docker push eu.gcr.io/vhdplus/storefront
