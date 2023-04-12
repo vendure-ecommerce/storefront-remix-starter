e581f7f6fc0314c123c08c1a6abb01338010fc55
Daniel Biegler <DanielBiegler@users.noreply.github.com>
2023-04-12 18:23:48 +0200

    add styling for disabled state

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -9,6 +9,7 @@ export function Button(
       className={clsx(
         'bg-gray-100 border rounded-md py-2 px-4 text-base font-medium text-black hover:text-white hover:bg-primary-600 focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-gray-800',
         'flex items-center justify-around gap-2',
+        'disabled:cursor-not-allowed disabled:bg-gray-200 disabled:text-gray-400',
         props.className,
       )}
     >
