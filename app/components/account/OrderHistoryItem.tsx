106e23a3daa7cd87ca76485d506a0b5355ef947f
Daniel Biegler <DanielBiegler@users.noreply.github.com>
2023-04-15 20:35:40 +0200

    add orderlines with product info, wip pagination, styles

--- app/components/account/OrderHistoryItem.tsx
+++ app/components/account/OrderHistoryItem.tsx
@@ -3,68 +3,143 @@ import { Button } from "~/components/Button";
 import { Price } from '~/components/products/Price';
 import { ActiveCustomerOrderListQuery } from "~/generated/graphql";
 import { OrderStateBadge } from "~/components/account/OrderStateBadge";
+import { ChevronRightIcon } from '@heroicons/react/24/solid';
+import { ArrowTopRightOnSquareIcon, EllipsisHorizontalCircleIcon, EllipsisVerticalIcon } from "@heroicons/react/24/outline";
 
 type OrderHistoryItemProps = {
     order?: NonNullable<ActiveCustomerOrderListQuery['activeCustomer']>['orders']['items'][number]
     isInitiallyExpanded?: boolean
+    areDetailsInitiallyExpanded?: boolean
 };
 
 
 export default function OrderHistoryItem({
     order,
     isInitiallyExpanded = false,
+    areDetailsInitiallyExpanded = false,
 }: OrderHistoryItemProps) {
 
     const [isExpanded, setIsExpanded] = useState<boolean>(isInitiallyExpanded);
+    const [areDetailsExpanded, setAreDetailsExpanded] = useState<boolean>(areDetailsInitiallyExpanded);
 
     return (
         <div className="my-10 first:mt-0 last:mb-0 border rounded-lg">
 
-        {/* Upper Summary */}
-        <div className="p-4 lg:p-6
+            {/* Upper Summary */}
+            <div className="p-4 lg:p-6
             flex flex-row justify-between items-center
-            bg-gray-50
+            bg-gray-50 border-b
         ">
-            {/* Infos */}
-            <div className="flex flex-col md:flex-row gap-8 md:gap-8 lg:gap-16">
-                {/* Info - Date */}
-                <div>
-                    <span className="block font-medium">Date placed</span>
-                    <span className="text-gray-500" title={(new Date(order?.orderPlacedAt)).toLocaleString()}>
-                        {order?.orderPlacedAt ? (new Date(order.orderPlacedAt)).toLocaleDateString() : '--'}
-                    </span>
+                {/* Infos */}
+                <div className="flex flex-col md:flex-row gap-4 md:gap-8 lg:gap-16 text-sm">
+                    {/* Info - Date */}
+                    <div>
+                        <span className="block font-medium">Date placed</span>
+                        <span className="text-gray-500" title={(new Date(order?.orderPlacedAt)).toLocaleString()}>
+                            {order?.orderPlacedAt ? (new Date(order.orderPlacedAt))
+                                .toLocaleDateString(undefined, { day: "numeric", month: "long", year: "numeric" })
+                                :
+                                '--'
+                            }
+                        </span>
+                    </div>
+                    {/* Info - Total sum */}
+                    <div>
+                        <span className="block font-medium">Total sum</span>
+                        <span className="text-gray-500">
+                            <Price currencyCode={order?.currencyCode} priceWithTax={order?.totalWithTax}></Price>
+                        </span>
+                    </div>
+                    {/* Info - Order number */}
+                    <div>
+                        <span className="block font-medium">Order number</span>
+                        <span className="text-gray-500">{order?.code || '--'}</span>
+                    </div>
                 </div>
-                {/* Info - Total sum */}
-                <div>
-                    <span className="block font-medium">Total sum</span>
-                    <span className="text-gray-500">
-                        <Price currencyCode={order?.currencyCode} priceWithTax={order?.totalWithTax}></Price>
-                    </span>
-                </div>
-                {/* Info - Order number */}
-                <div>
-                    <span className="block font-medium">Order number</span>
-                    <span className="text-gray-500">{order?.code || '--'}</span>
+
+                {/* Status + Actions */}
+                <div className="gap-4 lg:gap-6 flex flex-col items-end md:flex-row md:items-center self-start">
+                    <OrderStateBadge state={order?.state} />
+                    <Button className="bg-white text-sm" onClick={() => setIsExpanded(!isExpanded)}>
+                        <ChevronRightIcon className={`w-5 h-5 transition-transform duration-100 ${isExpanded && 'rotate-90'}`} />
+                    </Button>
                 </div>
             </div>
 
-            {/* Status + Actions */}
-            <div className="gap-4 lg:gap-6 flex flex-col items-end md:flex-row md:items-center self-start">
-                <OrderStateBadge state={order?.state}/>
-                <Button className="bg-white text-sm" onClick={() => setIsExpanded(!isExpanded)}>
-                    {isExpanded ? 'Less details' : 'More details'}
-                </Button>
-            </div>
-        </div>
+            {/* Collapsable details */}
+            {isExpanded && (
+                <div className="flex flex-col">
+                    {order?.lines.map((line, key) => (
+                        <div key={key} className="p-4 lg:p-6 border-b flex flex-row gap-8 justify-between">
+                            {/* Product */}
+                            <div className="inline-flex justify-center items-center justify gap-4">
+                                <img src={line.featuredAsset?.source} className="w-24 h-24 object-cover rounded-md" />
+                                <span className="flex flex-1 flex-col gap-0">
+                                    <span className="text-gray-500 text-xs uppercase tracking-wide">SKU # {line.productVariant.sku}</span>
+                                    <span className="text-black text-sm font-semibold mb-2 line-clamp-3 md:line-clamp-2 max-w-md" title={line.productVariant.name}>
+                                        {line.productVariant.name} Lorem ipsum dolor imet malo long name name i need more characters pls think about longer names is this enough?
+                                    </span>
+                                    <span className="text-gray-500 text-sm">
+                                        {line.quantity} item/s<span className="mx-3">-</span><Price currencyCode={line.productVariant.currencyCode} priceWithTax={line.discountedLinePriceWithTax}></Price>
+                                    </span>
+                                </span>
+                            </div>
+                            {/* Per product actions */}
+                            <div className="inline-flex flex-col lg:flex-row justify-center items-stretch md:items-center gap-4 lg:gap-6">
+                                {/* <Button disabled title="Not implemented"><span className="text-sm">Write a review</span></Button> */}
+                                {/* <Button disabled title="Not implemented"><span className="text-sm">Return or replace</span></Button> */}
+                                <Button disabled title="Not implemented" className="!p-1 md:!py-2 md:!px-4 !h-full md:!h-auto">
+                                    <span className="text-xs hidden md:block">Actions</span>
+                                    <EllipsisVerticalIcon className="w-5 h-5" />
+                                </Button>
+                            </div>
+                        </div>
+                    ))}
 
-        {/* Collapsable details */}
-        {isExpanded && order?.lines.map((line, key) => (
-        <div key={key} className="p-4 lg:p-6 border-t">
-            <span className="flex flex-row justify-center text-gray-300 font-bold select-none">
-                {line.productVariant.name}
-            </span>
-        </div>
-        ))}
+                    {/* Per order actions */}
+                    <div className="p-2 lg:py-3 lg:px-6 flex justify-end gap-2 lg:gap-6">
+                        <Button disabled title="Not implemented">
+                            <span className="text-xs">Get product support</span>
+                            {/* <ArrowTopRightOnSquareIcon className="w-4 h-4" /> */}
+                        </Button>
+                        <Button disabled title="Not implemented">
+                            <span className="text-xs">View invoice</span>
+                            <ArrowTopRightOnSquareIcon className="w-4 h-4" />
+                        </Button>
+                        <Button onClick={() => setAreDetailsExpanded(!areDetailsExpanded)}>
+                            <span className="text-xs">Detailed overview</span>
+                            <ChevronRightIcon className={`w-5 h-5 transition-transform duration-100 ${areDetailsExpanded && 'rotate-90'}`} />
+                        </Button>
+                    </div>
+
+                    {/* More details - Could be expanded with shipping adresses, payment option, etc. */}
+                    {areDetailsExpanded && (
+                        <div className="p-2 lg:p-3 grid grid-cols-2 gap-1 text-sm max-w-sm self-center md:self-end">
+                            <h6 className="font-medium col-span-full">Order summary</h6>
+                            <span>Item(s) Subtotal:</span>
+                            <span className="text-end">€TODO</span>
+
+                            <span>Shipping & handling:</span>
+                            <span className="text-end"><Price currencyCode={order?.currencyCode} priceWithTax={order?.shippingWithTax}></Price></span>
+
+                            <span>Total before tax:</span>
+                            <span className="text-end">€TODO</span>
+
+                            <span>Estimated tax:</span>
+                            <span className="text-end">€TODO</span>
+
+                            <span>Total:</span>
+                            <span className="text-end">€TODO</span>
+
+                            <span>Applied coupons:</span>
+                            <span className="text-end">-€TODO</span>
+
+                            <span className="font-medium">Grand total:</span>
+                            <span className="font-medium text-end"><Price currencyCode={order?.currencyCode} priceWithTax={order?.totalWithTax}></Price></span>
+                        </div>
+                    )}
+                </div>
+            )}
 
         </div>
     );
--- app/routes/account/history.tsx
+++ app/routes/account/history.tsx
@@ -1,7 +1,14 @@
 import { useLoaderData } from "@remix-run/react";
 import { DataFunctionArgs, json, redirect } from "@remix-run/server-runtime";
+import { withZod } from "@remix-validated-form/with-zod";
+import { z } from 'zod';
+import { ValidatedForm } from "remix-validated-form";
 import OrderHistoryItem from "~/components/account/OrderHistoryItem";
 import { getActiveCustomerOrderList } from "~/providers/customer/customer";
+import { Select } from "~/components/Select";
+import { useState } from "react";
+import { Input } from "~/components/Input";
+import { Button } from "~/components/Button";
 
 
 
@@ -14,21 +21,83 @@ export async function loader({ request }: DataFunctionArgs) {
 }
 
 
+export const orderPaginationValidator = withZod(
+    z.object({
+        limit: z.number({
+                required_error: "Limit is required",
+                invalid_type_error: "Limit must be a number",
+                coerce: true,
+            })
+            .min(10, { message: 'Limit must be at least 10' })
+            .max(50, { message: 'Maximum limit is 50' }),
+        offset: z.number({
+                required_error: "Offset is required",
+                invalid_type_error: "Offset must be a number",
+                coerce: true,
+            })
+            .min(0, { message: 'Offset must be at least 0' })
+    }),
+);
+
+
 export default function AccountHistory() {
     const { orderList } = useLoaderData<typeof loader>();
-
+    const [paginationOffset, setPaginationOffset] = useState<number>(0);
+    const [paginationLimit, setPaginationLimit] = useState<number>(10);
+    
     return (
         <div className="pt-10">
 
-        {orderList.items.length === 0 && (
-        <div className="pt-16 text-3xl text-center italic text-gray-300 select-none flex justify-center items-center">
-            Your future orders will appear here
-        </div>
-        )}
-        {orderList.items?.map(item => (
-            <OrderHistoryItem key={item.code} order={item} isInitiallyExpanded={true} />
-        ))}
-            
+            {/* Pagination Helper - Bottom too? Later. */}
+            <div className="flex flex-row justify-between items-center">
+                <span>Showing orders {paginationOffset+1} to {paginationOffset + orderList.items.length} of {orderList.totalItems}</span>
+                <ValidatedForm id="orderPagination"
+                    className="flex flex-col md:flex-row gap-4"
+                    validator={orderPaginationValidator}
+                    // method="post" // TODO
+                    onSubmit={() => { }} // TODO
+                    defaultValues={{
+                        offset: 0,
+                    }}
+                >
+                    <Select
+                        name="limit"
+                        required
+                        noPlaceholder
+                        value={paginationLimit}
+                        onChangeCapture={e => setPaginationLimit(parseInt(e.currentTarget.value))}
+                    >
+                        <option value={10}>10 per page</option>
+                        <option value={20}>20 per page</option>
+                        <option value={50}>50 per page</option>
+                    </Select>
+
+                    {/* TODO */}
+                    {/* <Button
+                        disabled={paginationOffset == 0}
+                        onClick={ () => alert("TODO") }
+                    >
+                        Previous
+                    </Button>
+                    <Button
+                        disabled={paginationOffset + paginationLimit >= orderList.totalItems}
+                        onClick={ () => alert("TODO") }
+                    >
+                        Next
+                    </Button> */}
+                </ValidatedForm>
+
+            </div>
+
+            {orderList.items.length === 0 && (
+                <div className="pt-16 text-3xl text-center italic text-gray-300 select-none flex justify-center items-center">
+                    Your future orders will appear here
+                </div>
+            )}
+            {orderList.items?.map(item => (
+                <OrderHistoryItem key={item.code} order={item} isInitiallyExpanded={true} />
+            ))}
+
         </div>
     )
 }
