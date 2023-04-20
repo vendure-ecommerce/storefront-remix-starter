25c6e54262a8f02a935a6ec1902c94d22509dcdd
Daniel Biegler <DanielBiegler@users.noreply.github.com>
2023-04-20 15:43:10 +0200

    add labels for default shipping & billing addresses

--- app/components/account/EditAddressCard.tsx
+++ app/components/account/EditAddressCard.tsx
@@ -24,26 +24,39 @@ export default function EditAddressCard({
           },
         )}
       >
-        <div className="flex flex-col">
-          <span className="text-left text-base-semi">{address.fullName}</span>
-          {address.company && (
-            <span className="text-small-regular text-gray-700">
-              {address.company}
-            </span>
-          )}
-          <div className="flex flex-col text-left text-base-regular mt-2">
-            <span>
-              {address.streetLine1}
-              {address.streetLine2 && <span>, {address.streetLine2}</span>}
-            </span>
-            <span>
-              {address.postalCode}, {address.city}
-            </span>
-            <span>
-              {address.province && `${address.province}, `}
-              {address.country?.code?.toUpperCase()}
-            </span>
+        <div className="flex justify-between">
+          {/* Customer Data Section */}
+          <div className="flex flex-col">
+            <span className="text-left text-base-semi">{address.fullName}</span>
+            {address.company && (
+              <span className="text-small-regular text-gray-700">
+                {address.company}
+              </span>
+            )}
+            <div className="flex flex-col text-left text-base-regular mt-2">
+              <span>
+                {address.streetLine1}
+                {address.streetLine2 && <span>, {address.streetLine2}</span>}
+              </span>
+              <span>
+                {address.postalCode}, {address.city}
+              </span>
+              <span>
+                {address.province && `${address.province}, `}
+                {address.country?.code?.toUpperCase()}
+              </span>
+            </div>
           </div>
+          {/* Default Shipping/Billing Section */}
+          {(address.defaultShippingAddress || address.defaultBillingAddress) && (
+          <div className='text-end text-gray-500 uppercase tracking-wider'>
+            <span className="block text-sm font-medium">Default</span>
+            <span className="block text-xs mt-1">
+              {address.defaultShippingAddress && "Shipping"}
+              {address.defaultShippingAddress && address.defaultBillingAddress && <><br/>&amp;&nbsp;</>}
+              {address.defaultBillingAddress && "Billing"}</span>
+          </div>
+          )}
         </div>
         <div className="flex items-center gap-x-4">
           <Link
--- app/generated/graphql.ts
+++ app/generated/graphql.ts
@@ -3337,7 +3337,7 @@ export type ActiveCustomerDetailsQuery = { __typename?: 'Query', activeCustomer?
 export type ActiveCustomerAddressesQueryVariables = Exact<{ [key: string]: never; }>;
 
 
-export type ActiveCustomerAddressesQuery = { __typename?: 'Query', activeCustomer?: { __typename?: 'Customer', id: string, addresses?: Array<{ __typename?: 'Address', id: string, company?: string | null, fullName?: string | null, streetLine1: string, streetLine2?: string | null, city?: string | null, province?: string | null, postalCode?: string | null, phoneNumber?: string | null, defaultShippingAddress?: boolean | null, country: { __typename?: 'Country', id: string, code: string, name: string } }> | null } | null };
+export type ActiveCustomerAddressesQuery = { __typename?: 'Query', activeCustomer?: { __typename?: 'Customer', id: string, addresses?: Array<{ __typename?: 'Address', id: string, company?: string | null, fullName?: string | null, streetLine1: string, streetLine2?: string | null, city?: string | null, province?: string | null, postalCode?: string | null, phoneNumber?: string | null, defaultShippingAddress?: boolean | null, defaultBillingAddress?: boolean | null, country: { __typename?: 'Country', id: string, code: string, name: string } }> | null } | null };
 
 export type ActiveCustomerOrderListQueryVariables = Exact<{
   orderListOptions?: InputMaybe<OrderListOptions>;
@@ -3850,6 +3850,7 @@ export const ActiveCustomerAddressesDocument = gql`
       }
       phoneNumber
       defaultShippingAddress
+      defaultBillingAddress
     }
   }
 }
