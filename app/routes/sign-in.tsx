a7899b40e94160c841926ba76f06ae826a6453da
Daniel Biegler <DanielBiegler@users.noreply.github.com>
2023-04-21 09:14:30 +0200

    Add login state loading indicators (#38)
    * add pending ui to login, placeholder with default data for convenience
    
    * update app styles

--- app/routes/sign-in.tsx
+++ app/routes/sign-in.tsx
@@ -1,8 +1,10 @@
-import { Form, Link, useActionData, useSearchParams } from '@remix-run/react';
+import { Link, useFetcher, useSearchParams } from '@remix-run/react';
 import { DataFunctionArgs, json, redirect } from '@remix-run/server-runtime';
 import { login } from '~/providers/account/account';
 import { ErrorResult } from '~/generated/graphql';
 import { XCircleIcon } from '@heroicons/react/24/solid';
+import { Button } from '~/components/Button';
+import { ArrowPathIcon } from '@heroicons/react/24/solid';
 
 export async function action({ params, request }: DataFunctionArgs) {
   const body = await request.formData();
@@ -24,7 +26,8 @@ export async function action({ params, request }: DataFunctionArgs) {
 
 export default function SignInPage() {
   const [searchParams] = useSearchParams();
-  const actionData = useActionData<ErrorResult>();
+  const login = useFetcher<ErrorResult>();
+
   return (
     <>
       <div className="flex flex-col justify-center py-12 sm:px-6 lg:px-8">
@@ -55,7 +58,7 @@ export default function SignInPage() {
                 Password: <span className="font-bold">test</span>
               </p>
             </div>
-            <Form className="space-y-6" method="post">
+            <login.Form method="post"><fieldset disabled={login.state !== 'idle'} className="space-y-6">
               <input
                 type="hidden"
                 name="redirectTo"
@@ -75,7 +78,9 @@ export default function SignInPage() {
                     type="email"
                     autoComplete="email"
                     required
-                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-primary-500 focus:border-primary-500 sm:text-sm"
+                    defaultValue="test@vendure.io"
+                    placeholder="Email address"
+                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-primary-500 focus:border-primary-500 sm:text-sm disabled:text-gray-400 disabled:bg-gray-200 disabled:cursor-not-allowed"
                   />
                 </div>
               </div>
@@ -94,7 +99,9 @@ export default function SignInPage() {
                     type="password"
                     autoComplete="current-password"
                     required
-                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-primary-500 focus:border-primary-500 sm:text-sm"
+                    placeholder="Password"
+                    defaultValue="test"
+                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-primary-500 focus:border-primary-500 sm:text-sm disabled:text-gray-400 disabled:bg-gray-200 disabled:cursor-not-allowed"
                   />
                 </div>
               </div>
@@ -105,7 +112,7 @@ export default function SignInPage() {
                     id="rememberMe"
                     name="rememberMe"
                     type="checkbox"
-                    className="h-4 w-4 text-primary-600 focus:ring-primary-500 border-gray-300 rounded"
+                    className="h-4 w-4 text-primary-600 focus:ring-primary-500 border-gray-300 rounded disabled:bg-gray-300 disabled:cursor-not-allowed"
                     defaultChecked
                   />
                   <label
@@ -126,7 +133,7 @@ export default function SignInPage() {
                 </div>
               </div>
 
-              {actionData && (
+              {login.data && login.state === 'idle' && (
                 <div className="rounded-md bg-red-50 p-4">
                   <div className="flex">
                     <div className="flex-shrink-0">
@@ -140,7 +147,7 @@ export default function SignInPage() {
                         We ran into a problem signing you in!
                       </h3>
                       <p className="text-sm text-red-700 mt-2">
-                        {actionData.message}
+                        {login.data.message}
                       </p>
                     </div>
                   </div>
@@ -148,14 +155,17 @@ export default function SignInPage() {
               )}
 
               <div>
-                <button
+                <Button
                   type="submit"
                   className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-primary-600 hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500"
                 >
-                  Sign in
-                </button>
+                  <span className='flex gap-4 items-center'>
+                    {login.state !== 'idle' && <ArrowPathIcon className="animate-spin h-5 w-5 text-gray-500" />}
+                    Sign in
+                  </span>
+                </Button>
               </div>
-            </Form>
+            </fieldset></login.Form>
           </div>
         </div>
       </div>
