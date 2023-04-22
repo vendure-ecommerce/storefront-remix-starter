6de1a445965310708862b2c883656fb5cf6bd6ec
Daniel Biegler <DanielBiegler@users.noreply.github.com>
2023-04-22 16:14:53 +0200

    add setting of default shipping/billing and address deletion

--- app/components/account/EditAddressCard.tsx
+++ app/components/account/EditAddressCard.tsx
@@ -1,21 +1,65 @@
-import { CreditCardIcon, PencilIcon, TrashIcon, TruckIcon } from '@heroicons/react/24/outline';
-import { Link } from '@remix-run/react';
+import { ArrowPathIcon, CreditCardIcon, PencilIcon, TrashIcon, TruckIcon } from '@heroicons/react/24/outline';
+import { Link, useFetcher } from '@remix-run/react';
 import clsx from 'clsx';
-import { Address } from '~/generated/graphql';
+import { useState } from 'react';
+import { Address, ErrorResult } from '~/generated/graphql';
+import { Button } from '../Button';
+import { ErrorMessage } from '../ErrorMessage';
+import { HighlightedButton } from '../HighlightedButton';
+import Modal from '../modal/Modal';
 
 type EditAddressProps = {
   address: Address;
   isActive?: boolean;
-  onClickRemove?: () => void;
 };
 
 export default function EditAddressCard({
   address,
   isActive = false,
-  onClickRemove,
 }: EditAddressProps) {
+
+  const setShipping = useFetcher();
+  const setBilling = useFetcher();
+  const deleteAddress = useFetcher<ErrorResult>();
+  const [isDeleteModalVisible, setDeleteModalVisible] = useState<boolean>(false);
+
   return (
     <>
+      {/* Note: Only allow closing when it isnt loading to prevent accidental closing via outside-click */}
+      <Modal isOpen={isDeleteModalVisible} close={() => setDeleteModalVisible(deleteAddress.state === 'idle' ? false : true)}>
+        <deleteAddress.Form method="post" preventScrollReset>
+          <Modal.Title>Remove Address</Modal.Title>
+          <Modal.Body>
+            <div className="space-y-4 my-4">
+              Do you want to remove this address?
+              <input type="hidden" name="id" value={address.id} />
+              {deleteAddress.data && (
+                <ErrorMessage
+                  heading='Address could not be removed'
+                  message={deleteAddress.data?.message ?? 'Something went wrong.'}
+                />
+              )}
+            </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button type="button"
+              onClick={() => setDeleteModalVisible(false)}
+              disabled={deleteAddress.state !== 'idle'}
+            >
+              Cancel
+            </Button>
+            <HighlightedButton
+              type="submit"
+              name='_action'
+              value='deleteAddress'
+              disabled={deleteAddress.state !== 'idle'}
+              isSubmitting={deleteAddress.state !== 'idle'}
+            >
+              Yes
+            </HighlightedButton>
+          </Modal.Footer>
+        </deleteAddress.Form>
+      </Modal>
       <div
         className={clsx(
           'border border-gray-200 p-5 min-h-[220px] h-full w-full flex flex-col justify-between gap-8 transition-colors',
@@ -70,20 +114,59 @@ export default function EditAddressCard({
               <PencilIcon className="w-4 h-4"></PencilIcon>
               Edit
             </Link>
-            <button
-              type="button"
+            <button type="button"
+              title="Delete this address"
               className="text-gray-700 flex items-center gap-x-2"
-              onClick={() => onClickRemove && onClickRemove()}
+              disabled={deleteAddress.state !== "idle"}
+              onClick={() => setDeleteModalVisible(true)}
             >
-              <TrashIcon className="w-4 h-4"></TrashIcon>
+              {deleteAddress.state === "idle" ?
+                <TrashIcon className="w-4 h-4"></TrashIcon>
+                :
+                <ArrowPathIcon className='w-4 h-4 animate-spin'></ArrowPathIcon>
+              }
               Remove
             </button>
           </div>
           {(!address.defaultShippingAddress || !address.defaultBillingAddress) && (
             <div>
               <span className='text-gray-500 flex gap-4'>
-                {!address.defaultShippingAddress && <button className='text-gray-700 flex items-center gap-2' title="Set as default shipping" onClick={() => alert("TODO")}><TruckIcon className="w-4 h-4"></TruckIcon>Shipping</button>}
-                {!address.defaultBillingAddress && <button className='text-gray-700 flex items-center gap-2' title="Set as default billing" onClick={() => alert("TODO")}><CreditCardIcon className="w-4 h-4"></CreditCardIcon>Billing</button>}
+                {/* Default shipping */}
+                {!address.defaultShippingAddress && (
+                  <setShipping.Form method='post'>
+                    <input type="hidden" name="id" value={address.id} />
+                    <button name="_action" value="setDefaultShipping" type="submit"
+                      title="Set as default shipping address"
+                      className='text-gray-700 flex items-center gap-2'
+                      disabled={setShipping.state !== "idle"}
+                    >
+                      {setShipping.state === "idle" ?
+                        <TruckIcon className="w-4 h-4"></TruckIcon>
+                        :
+                        <ArrowPathIcon className='w-4 h-4 animate-spin'></ArrowPathIcon>
+                      }
+                      Shipping
+                    </button>
+                  </setShipping.Form>
+                )}
+
+                {!address.defaultBillingAddress && (
+                  <setBilling.Form method='post'>
+                    <input type="hidden" name="id" value={address.id} />
+                    <button name="_action" value="setDefaultBilling" type="submit"
+                      title="Set as default billing address"
+                      className='text-gray-700 flex items-center gap-2'
+                      disabled={setBilling.state !== "idle"}
+                    >
+                      {setBilling.state === "idle" ?
+                        <CreditCardIcon className="w-4 h-4"></CreditCardIcon>
+                        :
+                        <ArrowPathIcon className='w-4 h-4 animate-spin'></ArrowPathIcon>
+                      }
+                      Billing
+                    </button>
+                  </setBilling.Form>
+                )}
               </span>
             </div>
           )}
