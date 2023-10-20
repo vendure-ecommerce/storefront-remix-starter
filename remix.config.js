60294982e86b2ec8502cc0c316d3d09fa29c3e1b
Michael Bromley <michael@michaelbromley.co.uk>
2023-10-20 10:37:43 +0200

    chore: Update route config

--- remix.config.js
+++ remix.config.js
@@ -1,3 +1,5 @@
+const { createRoutesFromFolders } = require('@remix-run/v1-route-convention');
+
 /**
  * @type {import('@remix-run/dev').AppConfig}
  */
@@ -27,6 +29,11 @@ const devConfig = {
     v2_normalizeFormMethod: true,
     v2_meta: true,
     v2_headers: true,
+    v2_routeConvention: true,
+  },
+  routes(defineRoutes) {
+    // uses the v1 convention, works in v1.15+ and v2
+    return createRoutesFromFolders(defineRoutes);
   },
 };
 
