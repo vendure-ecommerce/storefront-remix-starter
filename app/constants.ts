a57402e5f4fe21c48aa8a5cc9eab9cbbe4f440e5
noetamas90 <noe.tamas90@gmail.com>
2024-08-07 12:50:05 +0200

    Update demo url to localhost:3000

--- app/constants.ts
+++ app/constants.ts
@@ -1,7 +1,7 @@
 export const APP_META_TITLE = 'Vendure Remix Storefront';
 export const APP_META_DESCRIPTION =
   'A headless commerce storefront starter kit built with Remix & Vendure';
-export const DEMO_API_URL = 'https://readonlydemo.vendure.io/shop-api';
+export const DEMO_API_URL = 'http://localhost:3000/shop-api';
 export let API_URL =
   typeof process !== 'undefined'
     ? process.env.VENDURE_API_URL ?? DEMO_API_URL
