8c4ca8f0c2d8b4ed8ada9066156bcebe4c7f3c57
kinstadtnerbalint <kinstadtnerbalint@gmail.com>
2024-08-26 14:46:05 +0200

    fix: revert sheet animation

--- app/components/ui-custom/MySheet.tsx
+++ app/components/ui-custom/MySheet.tsx
@@ -30,17 +30,16 @@ const SheetOverlay = React.forwardRef<
 SheetOverlay.displayName = SheetPrimitive.Overlay.displayName;
 
 const sheetVariants = cva(
-  'fixed z-50 gap-4 bg-white p-0 shadow-lg transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out  data-[state=open]:duration-500', // data-[state=closed]:duration-300
+  'fixed z-50 gap-4 bg-white p-0 shadow-lg transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500',
   {
     variants: {
       side: {
-        top: 'inset-x-0 top-0 border-b data-[state=open]:slide-in-from-top', // data-[state=closed]:slide-out-to-top
+        top: 'inset-x-0 top-0 border-b data-[state=closed]:slide-out-to-top data-[state=open]:slide-in-from-top',
         bottom:
-          'inset-x-0 bottom-0 border-t  data-[state=open]:slide-in-from-bottom', // data-[state=closed]:slide-out-to-bottom
-        left: 'inset-y-0 left-0 h-full w-3/4 border-r  data-[state=open]:slide-in-from-left sm:max-w-sm', // data-[state=closed]:slide-out-to-left
+          'inset-x-0 bottom-0 border-t data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom',
+        left: 'inset-y-0 left-0 h-full w-3/4 border-r data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left sm:max-w-sm',
         right:
-          'inset-y-0 right-0 h-full w-3/4 border-l data-[state=open]:slide-in-from-right sm:max-w-sm',
-        // data-[state=closed]:slide-out-to-right
+          'inset-y-0 right-0 h-full w-3/4  border-l data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right sm:max-w-sm',
       },
     },
     defaultVariants: {
