95786e01abfaf2971a8b01b4b49048dd6fc75fb2
Kevin Coscarelli <kevinsl1991@gmail.com>
2024-12-24 13:06:15 +0100

    Fix for nested routes rendering issue

--- remix.config.js
+++ remix.config.js
@@ -1,5 +1,3 @@
-import { createRoutesFromFolders } from '@remix-run/v1-route-convention';
-
 /**
  * @type {import('@remix-run/dev').AppConfig}
  */
@@ -11,10 +9,6 @@ const commonConfig = {
     '@remix-validated-form/with-zod',
   ],
   tailwind: true,
-  routes(defineRoutes) {
-    // uses the v1 convention, works in v1.15+ and v2
-    return createRoutesFromFolders(defineRoutes);
-  },
 };
 
 /**
