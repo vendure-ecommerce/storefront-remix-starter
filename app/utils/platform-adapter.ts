a59e193bfdb4fea3d74c558c337227bb7466bc2e
axbegue <github@axbp.com>
2025-02-12 04:14:52 -0500

    refactor: Update safeRequireNodeDependency to use Vite ignore comment for module imports

--- app/utils/platform-adapter.ts
+++ app/utils/platform-adapter.ts
@@ -3,5 +3,5 @@ export const IS_CF_PAGES = typeof process === 'undefined';
 // This hack is to prevent `node` modules/packages being bundled in the
 // Cloudflare Pages context, which causes an error.
 export async function safeRequireNodeDependency(module: string) {
-  return import(module.split('').join(''));
+  return import(/* @vite-ignore */module.split('').join(''));
 }
