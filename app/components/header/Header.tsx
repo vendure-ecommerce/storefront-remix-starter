6623aa987cfad5e5006338f0f40515c833598bea
JuanFdez12 <juanfdez1112@gmail.com>
2025-08-28 21:37:43 -0600

    Remove top bar, re-order nav bar

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,5 +1,5 @@
 import { Link, useLoaderData } from '@remix-run/react';
-import { ShoppingBagIcon } from '@heroicons/react/24/outline';
+import { ShoppingCartIcon } from '@heroicons/react/24/outline';
 import { SearchBar } from '~/components/header/SearchBar';
 import { useRootLoader } from '~/utils/use-root-loader';
 import { UserIcon } from '@heroicons/react/24/solid';
@@ -18,50 +18,20 @@ export function Header({
   const isSignedIn = !!data.activeCustomer.activeCustomer?.id;
   const isScrollingUp = useScrollingUp();
   const { t } = useTranslation();
-
+  
+  //posibles opciones de color  #14452F #03453D  #073830ee 
+  
   return (
     <header
       className={classNames(
         isScrollingUp ? 'sticky top-0 z-10 animate-dropIn' : '',
-        'bg-gradient-to-r from-zinc-700 to-gray-900 shadow-lg transform shadow-xl',
+        'bg-[#073830ee]',
       )}
     >
-      <div className="bg-zinc-100 text-gray-600 shadow-inner text-center text-sm py-2 px-2 xl:px-0">
-        <div className="max-w-6xl mx-2 md:mx-auto flex items-center justify-between">
-          <div>
-            <p className="hidden sm:block">
-              {t('vendure.exclusive')}{' '}
-              <a
-                href="https://github.com/vendure-ecommerce/storefront-remix-starter"
-                target="_blank"
-                className="underline"
-              >
-                {t('vendure.repoLinkLabel')}
-              </a>
-            </p>
-          </div>
-          <div>
-            <Link
-              to={isSignedIn ? '/account' : '/sign-in'}
-              className="flex space-x-1"
-            >
-              <UserIcon className="w-4 h-4"></UserIcon>
-              <span>
-                {isSignedIn ? t('account.myAccount') : t('account.signIn')}
-              </span>
-            </Link>
-          </div>
-        </div>
-      </div>
       <div className="max-w-6xl mx-auto p-4 flex items-center space-x-4">
-        <h1 className="text-white w-10">
+        <h1 className="font-serif text-[#FFFFFF] text-[24px] w-10 text_x mr-14">
           <Link to="/">
-            <img
-              src="/cube-logo-small.webp"
-              width={40}
-              height={31}
-              alt={t('commmon.logoAlt')}
-            />
+            Folklor
           </Link>
         </h1>
         <div className="flex space-x-4 hidden sm:block">
@@ -76,16 +46,16 @@ export function Header({
             </Link>
           ))}
         </div>
-        <div className="flex-1 md:pr-8">
+        <div className="flex-1 md:pr-2">
           <SearchBar></SearchBar>
         </div>
         <div className="">
           <button
-            className="relative w-9 h-9 bg-white bg-opacity-20 rounded text-white p-1"
+            className="relative w-9 h-9 rounded text-[#FFFFFF] p-1"
             onClick={onCartIconClick}
             aria-label="Open cart tray"
           >
-            <ShoppingBagIcon></ShoppingBagIcon>
+            <ShoppingCartIcon></ShoppingCartIcon>
             {cartQuantity ? (
               <div className="absolute rounded-full -top-2 -right-2 bg-primary-600 min-w-6 min-h-6 flex items-center justify-center text-xs p-1">
                 {cartQuantity}
@@ -93,7 +63,18 @@ export function Header({
             ) : (
               ''
             )}
-          </button>
+          </button>         
+        </div>
+        <div>
+           <Link
+            to={isSignedIn ? '/account' : '/sign-in'}
+            className="flex space-x-1 text-[#FFF] hover:text-red" 
+          >
+            <UserIcon className="w-4 h-7"></UserIcon>
+            <span>
+              {isSignedIn ? t('account.myAccount') : t('account.signIn')}
+            </span>
+          </Link>
         </div>
       </div>
     </header>
